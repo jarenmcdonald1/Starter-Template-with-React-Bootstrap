f5f32aa4271a7e8a6288afe0c6674ac0078679a4
jmcdonald1 <jaren.mcd@gmail.com>
2020-05-20 12:41:36 -0400

    added sass plugin

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,7 +11,7 @@ import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import 'bootstrap/dist/css/bootstrap.min.css'
-import '../css/custom.css'
+import '../css/custom.scss'
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
